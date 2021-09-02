0f24fd94a5350c1268d7189ed5e7a7fd7918320d
Daniel Santa Rosa <iel_182@hotmail.com>
2021-09-01 21:25:46 -0300

    http

---
+++ http/serverdb/cliente.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Usuario struct {
+	Id   int    `json:"id"`
+	Nome string `json:"nome"`
+}
+
+func UsuarioHandler(w http.ResponseWriter, r *http.Request) {
+	sid := strings.TrimPrefix(r.URL.Path, "/usuarios/")
+	id, _ := strconv.Atoi(sid)
+
+	switch {
+	case r.Method == "GET" && id > 0:
+		usuarioPorId(w, r, id)
+	case r.Method == "GET":
+		usuarioTodos(w, r)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "Desculpa... :(")
+	}
+}
+
+func usuarioPorId(w http.ResponseWriter, r *http.Request, id int) {
+	db, err := sql.Open("mysql", "root:123456@/cursogo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var c Usuario
+	db.QueryRow("select * from usuarios where id = ?", id).Scan(&c)
+
+	json, _ := json.Marshal(c)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, string(json))
+}
+
+func usuarioTodos(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql", "root:123456@/cursogo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, _ := db.Query("select * from usuarios")
+	defer rows.Close()
+
+	var usuarios []Usuario
+	for rows.Next() {
+		var usuario Usuario
+		rows.Scan(&usuario)
+		usuarios = append(usuarios, usuario)
+	}
+
+	json, _ := json.Marshal(usuarios)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, string(json))
+}
